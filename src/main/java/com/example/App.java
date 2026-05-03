5786c6b1ee2e4c835fd4ef74b9e9a6a2a51bac79
bishnu <bishnu@gmail.com>
2026-05-03 17:29:38 +0530

    Example Project for DevOps

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class App {
+
+	public static void main(String[] args) {
+System.out.println("Hello, Maven Project!");
+        
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+
+	}
+
+}
