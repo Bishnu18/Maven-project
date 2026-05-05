13238c8a46be845e2d21dbc71c41f56f1f581acf
Bishnu Basudev Patra <141321371+Bishnu18@users.noreply.github.com>
2026-05-05 14:14:04 +0530

    Implement user input for arithmetic calculations
    Added user input functionality for arithmetic operations.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,8 +5,15 @@ public class App {
 	public static void main(String[] args) {
 System.out.println("Hello, Maven Project!");
         
-        // int a = 15;
-        // int b = 5;
+         Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+		
 
         System.out.println("Addition: " + (a + b));
         System.out.println("Subtraction: " + (a - b));
